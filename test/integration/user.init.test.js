2a3950ac873db132254ee909bccb0e09e481f631
chawin <jeffychawin@gmail.com>
2022-06-15 10:47:23 +0700

    Finish Integration Test

--- test/integration/user.init.test.js
+++ test/integration/user.init.test.js
@@ -4,6 +4,9 @@ import mongoose from 'mongoose'
 import userRouter from '../../src/routes/user'
 import userModel from '../../src/models/user'
 
+/* eslint implicit-arrow-linebreak: ["error", "below"] */
+/* eslint no-underscore-dangle: ["error", { "allow": ["_id"] }] */
+
 // connect mongo db
 const database = 'mongodb://localhost:27017'
 const option = {
@@ -35,33 +38,125 @@ describe('user router', () => {
   })
 
   describe('route API GET /user', () => {
-    it('username is not found in database', () => supertest(app)
-      .get('/user')
-      .query({ username: 'notfoundusername' })
-      .then((response) => {
-        expect(response.statusCode).toBe(404)
-        expect(response.body).toEqual({
-          message: 'Not found',
-        })
-      }))
+    it('username is not found in database', () =>
+      supertest(app)
+        .get('/user')
+        .query({ username: 'notfoundusername' })
+        .then((response) => {
+          expect(response.statusCode).toBe(404)
+          expect(response.body).toEqual({
+            message: 'Not found',
+          })
+        }))
 
-    it('username is found in database', () => supertest(app)
-      .get('/user')
-      .query({ username: 'testusername' })
-      .then((response) => {
-        /* eslint no-underscore-dangle: ["error", { "allow": ["_id"] }] */
-        delete response.body._id
-        delete response.body.createdAt
-        delete response.body.updatedAt
+    it('username is found in database', () =>
+      supertest(app)
+        .get('/user')
+        .query({ username: 'testusername' })
+        .then((response) => {
+          delete response.body._id
+          delete response.body.createdAt
+          delete response.body.updatedAt
 
-        expect(response.statusCode).toBe(200)
-        expect(response.body).toEqual({
-          username: 'testusername',
+          expect(response.statusCode).toBe(200)
+          expect(response.body).toEqual({
+            username: 'testusername',
+            firstname: 'testfirstname',
+            lastname: 'testlastname',
+            gender: 'MALE',
+            job: 'PROGRAMMER',
+          })
+        }))
+  })
+
+  describe('route API POST /user', () => {
+    afterAll(async () => {
+      await userModel.remove({ username: 'newtestusername' })
+    })
+
+    it('should return data correctly when create complete', () =>
+      supertest(app)
+        .post('/user')
+        .send({
+          username: 'newtestusername',
           firstname: 'testfirstname',
           lastname: 'testlastname',
-          gender: 'MALE',
-          job: 'PROGRAMMER',
         })
-      }))
+        .then((response) => {
+          delete response.body._id
+          delete response.body.createdAt
+          delete response.body.updatedAt
+
+          expect(response.statusCode).toBe(200)
+          expect(response.body).toEqual({
+            username: 'newtestusername',
+            firstname: 'testfirstname',
+            lastname: 'testlastname',
+            gender: 'MALE',
+            job: 'PROGRAMMER',
+          })
+        }))
+  })
+
+  describe('route API PUT /user', () => {
+    it('should update and return data correctly when found username', () =>
+      supertest(app)
+        .put('/user')
+        .send({
+          username: 'testusername',
+          firstname: 'firstnameedit',
+          lastname: 'lastnameedit',
+        })
+        .then((response) => {
+          delete response.body._id
+          delete response.body.createdAt
+          delete response.body.updatedAt
+
+          expect(response.statusCode).toBe(200)
+          expect(response.body).toEqual({
+            username: 'testusername',
+            firstname: 'firstnameedit',
+            lastname: 'lastnameedit',
+            gender: 'MALE',
+            job: 'PROGRAMMER',
+          })
+        }))
+
+    it('should return null when not found username', () =>
+      supertest(app)
+        .put('/user')
+        .send({
+          username: 'testusername2',
+          firstname: 'firstnameedit',
+          lastname: 'lastnameedit',
+        })
+        .then((response) => {
+          expect(response.statusCode).toBe(200)
+          expect(response.body).toBeNull()
+        }))
+  })
+
+  describe('route API DELETE /user', () => {
+    it('should delete data correctly when found username', () =>
+      supertest(app)
+        .delete('/user')
+        .send({
+          username: 'testusername',
+        })
+        .then((response) => {
+          expect(response.statusCode).toBe(200)
+          expect(response.body).toHaveProperty('deletedCount', 1)
+        }))
+
+    it('should not delete data when not found username', () =>
+      supertest(app)
+        .delete('/user')
+        .send({
+          username: 'testusername2',
+        })
+        .then((response) => {
+          expect(response.statusCode).toBe(200)
+          expect(response.body).toHaveProperty('deletedCount', 0)
+        }))
   })
 })
